036d9bc4b84cf4dd30549244849e58f30330bf32
Tanuja Lawaniya <62615222+TanujaLawaniya@users.noreply.github.com>
2021-01-10 08:45:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,6 +29,22 @@ function draw() {
   //add styles here
 
 }
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
 
 
 
